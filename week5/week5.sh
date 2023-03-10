4133a4f81c8a74fa563bff194969bd4569fdbecd
andrew-kh <andrey.khuhlin@gmail.com>
2023-03-10 22:46:53 +0300

    Create week5.sh

---
+++ week5/week5.sh
@@ -0,0 +1,7 @@
+export SPARK_HOME="/mnt/c/Users//vs-projects/de_zoomcamp/Week5/spark-3.3.2-bin-hadoop3"
+export PATH="${SPARK_HOME}/bin:${PATH}"
+spark-shell
+
+
+export PYTHONPATH="${SPARK_HOME}/python/:$PYTHONPATH"
+export PYTHONPATH="${SPARK_HOME}/python/lib/py4j-0.10.9.5-src.zip:$PYTHONPATH"
